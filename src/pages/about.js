852ed5ae21c68cc286e9fc180487622bd3f05884
Ludovic DURAND <ludurand07@gmail.com>
2022-11-02 17:15:10 +0100

    Ajout de page about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,21 +1,20 @@
 // Step 1: Import React
 import * as React from 'react'
 import { Link } from 'gatsby'
-
-
 // Step 2: Define your component
-const IndexPage = () => {
+const AboutPage = () => {
   return (
     <main>
-      <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <Link to="/about">About</Link>
+      <h1>A propos de moi!</h1>
+      <p>Je suis un bidouilleur en tous genrs et j'essay de dompté Gatsby</p>
+      <Link to="/">Menu</Link>
     </main>
   )
 }
 
+
 // You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>about</title>
 
 // Step 3: Export your component
-export default IndexPage
+export default AboutPage
